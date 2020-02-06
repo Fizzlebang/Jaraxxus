f27c9fd83f981483be3b5ecf84a0e9135fb36709
Ash Wang <ash6260@gmail.com>
2020-02-06 14:09:36 -0500

    Formatted to align with project style

--- server/src/lib/internal/pullData.js
+++ server/src/lib/internal/pullData.js
@@ -1,36 +1,33 @@
-var request = require('request');
-
-/**
- * returns a request object of a twitch VOD's metadata based on the passed VOD id
- * 
- * @param {String} id
- */
-exports.getVODMeta = function (vodID){
-    return getTwitchData(`https://api.twitch.tv/kraken/videos/${vodID}`);
-}
-
-/**
- * returns a request object of a twitch VOD's chat log based on the passed VOD id
- * 
- * @param {String} id
- */
-exports.getVODChat = function (vodID){
-    return getTwitchData(`https://api.twitch.tv/kraken/videos/${vodID}/comments`);
-}
-
+const request = require('request');
 
 /**
  * returns a request object of a twitch api call based on the passed url
- * 
  * @param {String} id
  */
-function getTwitchData(url){
-    var options = {
-        url: url,
-        headers: {
-            'Client-ID': process.env.TWITCH_CLIENT_ID,
-            'Accept': 'application/vnd.twitchtv.v5+json'
-        }
+function getTwitchData(url) {
+  const options = {
+    url,
+    headers: {
+      'Client-ID': process.env.TWITCH_CLIENT_ID,
+      Accept: 'application/vnd.twitchtv.v5+json'
     }
-    return request.get(options);
+  };
+  return request.get(options);
 }
+
+/**
+ * returns a request object of a twitch VOD's metadata based on the passed VOD id
+ * @param {String} id
+ */
+exports.getVODMeta = function(vodID) {
+  return getTwitchData(`https://api.twitch.tv/kraken/videos/${vodID}`);
+};
+
+/**
+ * returns a request object of a twitch VOD's chat log based on the passed VOD id
+ *
+ * @param {String} id
+ */
+exports.getVODChat = function(vodID) {
+  return getTwitchData(`https://api.twitch.tv/kraken/videos/${vodID}/comments`);
+};
